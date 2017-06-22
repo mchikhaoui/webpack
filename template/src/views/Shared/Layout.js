b2f0012a14330014b70c794473e534dff3dfb691
Maher CHIKHAOUI <mchikhaoui@outlook.com>
2017-06-22 21:06:26 +0200

    update dependencies

--- template/src/views/Shared/Layout.js
+++ template/src/views/Shared/Layout.js
@@ -1,5 +1,5 @@
 import template from './layout.html'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-import '@/assets/css/layout.css'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+import /* webpackChunkName: "layout" */'@/assets/css/layout.css'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 
 export default {
   template: template{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
